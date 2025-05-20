b7431ca563437383d9240f9b2b1f7f635ba4fccd
Mohamad Siyam <siyam_2020679@fot.ruh.ac.lk>
2025-05-20 15:05:44 +0530

    chore : modifed the login page

--- ems-frontend/src/pages/Login.jsx
+++ ems-frontend/src/pages/Login.jsx
@@ -26,17 +26,17 @@ function Login() {
                 localStorage.setItem('userId', res.data.id);
                 localStorage.setItem('userEmail', res.data.email);
                 localStorage.setItem('userRole', res.data.role);
+                localStorage.setItem('userName', res.data.name);
+                // console.log(res.data.role);
+
+                if (res.data.role === 'ADMIN') navigate('/admin');
+                else if (res.data.role === 'EMPLOYEE') navigate('/employee');
+                else alert('Unknown role');
             }else{
                 alert('Invalid Credentials');
-                return;
             }
-
-            if (res.data.role === 'ADMIN') navigate('/admin');
-            else if (res.data.role === 'EMPLOYEE') navigate('/employee');
-            else alert('Unknown role');
-
         } catch (err) {
-            alert(err.message);
+            alert('Login failed: '+ err.message);
         }
 
     };
@@ -46,11 +46,11 @@ function Login() {
             <h1>Login</h1>
             <form onSubmit={handleSubmit}>
                 <div className="input-group">
-                    <label for="email">Email</label>
+                    <label htmlFor="email">Email</label>
                     <input type="email" id='email' name='email' placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} required />
                 </div>
                 <div className="input-group">
-                    <label for="password">Password</label>
+                    <label htmlFor="password">Password</label>
                     <input type="password" id='password' name='password' placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} required />
                 </div>
                 <center>

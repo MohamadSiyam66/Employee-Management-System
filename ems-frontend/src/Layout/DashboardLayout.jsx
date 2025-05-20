ee88f0c8f08bcd9686b5d003e9f66be3bb9f46df
Mohamad Siyam <siyam_2020679@fot.ruh.ac.lk>
2025-05-20 18:36:35 +0530

    chore : updated dhashboard layout

--- ems-frontend/src/Layout/DashboardLayout.jsx
+++ ems-frontend/src/Layout/DashboardLayout.jsx
@@ -1,13 +1,16 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Sidebar from '../components/Sidebar';
 import Header from '../components/Header';
 
 const DashboardLayout = () => (
-    <div style={{ display: 'flex' }}>
-        <Sidebar />
-        <section>
-            <Outlet />
-        </section>
+    <div style={{ display: 'flex', flexDirection: 'column', height: '90vh' }}>
+        <Header />
+        <div style={{ display: 'flex', flex: 1 }}>
+            <Sidebar />
+            <main style={{ flex: 1, padding: '20px', overflowY: 'auto' }}>
+                <Outlet />
+            </main>
+        </div>
     </div>
 );
 
